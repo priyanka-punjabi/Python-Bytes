9cea6c94fab627d02dc9db67176615d33e5d3237
Priyanka Punjabi <44215717+priyanka-punjabi@users.noreply.github.com>
2019-02-23 18:31:23 -0500

    A non negative integer variable V is given. There are two actions available that modify its value. 1. If V is odd, subtract 1 from it. 2. If V is even, divide it by 2. These actions are performed until the value of V becomes 0. Return the number of steps after which the value of V becomes 0. Accept a binary string S, which would be the binary equivalent of the desired number V.

---
+++ Tests/mathAlgo.py
@@ -0,0 +1,18 @@
+"""
+@author: Priyanka Punjabi
+"""
+
+
+def solution(S):
+    v = int(S, 2)
+    count = 0
+    while v != 0:
+        if v % 2 == 0:
+            v = v / 2
+        else:
+            v = v - 1
+        count += 1
+    return count
+
+
+print(solution('011100'))
